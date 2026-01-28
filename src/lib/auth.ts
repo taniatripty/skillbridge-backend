7f541ac69f6bd43b9630d0a0282c20f74d1aeb77
taniatripty <taniatripty381@gmail.com>
2026-01-28 22:52:49 +0600

    middleware part committed

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -151,4 +151,12 @@ export const auth = betterAuth({
       }
     },
   },
+  socialProviders: {
+    google: {
+      prompt: "select_account consent",
+      accessType: "offline",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  }
 });
---
+++ src/middleware/authmiddleware.ts
@@ -0,0 +1,67 @@
+import { NextFunction, Request, Response } from "express";
+import { auth } from "../lib/auth";
+
+
+export enum UserRoles {
+     USER = "USER",
+    ADMIN = "ADMIN"
+}
+declare global{
+    namespace Express{
+        interface Request{
+            user?:{
+                id:string;
+                email:string;
+                name:string;
+                role:UserRoles;
+                emailVerified: boolean;
+
+
+            }
+        }
+    }
+}
+
+const authmiddleware = (...roles: UserRoles[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+  try {
+      const session = await auth.api.getSession({
+      headers: req.headers as any,
+    });
+    if (!session) {
+      return res.status(401).json({
+        success: false,
+        message: "You are not authorized!",
+      });
+    }
+
+    if (!session.user.emailVerified) {
+      return res.status(403).json({
+        success: false,
+        message: "Email verification required. Please verfiy your email!",
+      });
+    }
+    req.user={
+        id:session.user.id,
+        email:session.user.email,
+        name:session.user.name,
+        role:session.user.role as UserRoles,
+        emailVerified:session.user.emailVerified
+
+    }
+
+     if (roles.length && !roles.includes(req.user.role as UserRoles)) {
+                return res.status(403).json({
+                    success: false,
+                    message: "Forbidden! You don't have permission to access this resources!"
+                })
+            }
+    next()
+  } catch (error) {
+    next(error)
+    
+  }
+  };
+};
+
+export default authmiddleware;
