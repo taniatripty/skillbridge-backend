8ce4a2b394da5a5f7a5296275e0099e09b41c14b
taniatripty <taniatripty381@gmail.com>
2026-01-28 20:14:13 +0600

    add better auth user model schema

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from'express'
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './lib/auth';
 const app=express()
 app.use(express.json())
-
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.get('/',(req,res)=>{
 res.send('SkillBridge Backend')
 })
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,31 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-// If your Prisma file is located elsewhere, you can change the path
-
-
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "STUDENT",
+        required: true,
+      },
+       phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+
+  emailAndPassword: {
+    enabled: true,
+  },
 });
